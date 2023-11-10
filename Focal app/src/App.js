83b12beca30282fd3085144fdc63ebe5161a0e44
kosaysh123 <qosaish703@gmail.com>
2023-11-11 00:52:47 +0300

    first uploed

--- Focal app/src/App.js
+++ Focal app/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import SkillsList from "../../../Focal_app_main/Focal app/src/modules/JobSeeker/components/SkillsList/SkillsList"
+import Experience from "../../../Focal_app_main/Focal app/src/modules/JobSeeker/components/Experience/Experience"
+import LanguagesList from "../../../Focal_app_main/Focal app/src/modules/JobSeeker/components/LanguagesList/LanguagesList"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+        <SkillsList />
+       <Experience />
+       <LanguagesList/>
+    </>
+
+    
   );
 }
 
---
+++ Focal app/src/modules/JobSeeker/components/Exp/Exp.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+// import StarRatings from "react-star-ratings";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Skill = ({ id, name1, name2, onEdit, onDelete,inputdata, startDate }) => {
+    const handleEditClick = () => {
+      onEdit(id);
+    };
+  
+    const handleDeleteClick = () => {
+      onDelete(id);
+    };
+    console.log(inputdata)
+    console.log(startDate)
+    return (
+    <div className="KO-card-item">
+    <div className="KO-main-text">
+    <h1 className="KO-heding">{name1}</h1>
+    <p className="KO-pargrath">{name2}</p>
+    </div>
+    <div className="KO-edit">
+        <div className="KO-main-icon">
+      <FontAwesomeIcon icon={faPenToSquare} className="KO-Update" onClick={handleEditClick}/>
+      <FontAwesomeIcon icon={faTrash} className="KO-Delete" onClick={handleDeleteClick}/>
+        </div>
+        <p className="KO-date">{startDate} To {inputdata}</p>
+    </div>
+  </div>
+    );
+  };
+
+export default Skill
