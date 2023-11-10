80df5c246e78efd703e9f4a7b2fa70fe2a9961c4
Aya1Khlif <ayakhlife222@gmail.com>
2023-11-11 00:04:33 +0300

    Home

---
+++ Focal app/src/modules/JobSeeker/components/Section1/Section1.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Section1.css";
+import "../style/style.css";
+function Section1() {
+  return (
+    <>
+      <div className="section1">
+        <div className="AK">
+          <h1 className="h1">What is Focal App?</h1>
+          <p>
+            The first platform in Syria that brings together companies,
+            business owners, employees, and freelancers, in addition to
+            bloggers. <p> This platform seeks to develop youth and secure fair job
+            opportunities. </p>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Section1;
---
+++ Focal app/src/modules/JobSeeker/components/Section2/section2.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./section2.css";
+import "../style/style.css";
+function Section2() {
+  return (
+    <>
+      <div className="section2">
+        <div className="AK2">
+          <h1>Who We Are?</h1>
+          <p>
+            WE are a family of worldwide 75 specialists of storytellers and
+            marketers, brand builders, and designers, in addition to programmers
+            and developers.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Section2;
+
