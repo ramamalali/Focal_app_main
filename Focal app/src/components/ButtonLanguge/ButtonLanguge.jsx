bc6f9da6d8db806a178dd12672b33015bc8d5058
ranimRefaie <ranemalre@gmail.com>
2023-11-11 12:45:49 +0300

    Ranim

---
+++ Focal app/src/components/ButtonLanguge/ButtonLanguge.jsx
@@ -0,0 +1,15 @@
+import "./ButtonLanguge.css";
+
+const ButtonLanguge = () => {
+  return (
+    <div className="rn-group-languge">
+      <button>
+        <span className="rn-arabic-btn">AR</span>
+        <span className="rn-english-btn">En</span>
+      </button>
+      {/* <button className="rn-english-btn">EN</button> */}
+    </div>
+  );
+};
+
+export default ButtonLanguge;
---
+++ Focal app/src/components/NavBarAll/NavBlogRegster.jsx
@@ -0,0 +1,53 @@
+import "./NavBar.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logo from "./img/Layer 2.svg";
+import DropDown from "../DropDown/DropDown";
+import ButtonLanguge from "../ButtonLanguge/ButtonLanguge";
+import ButtonNotifcation from "../ButtonNotifcation/ButtonNotifcation";
+import icon from "../ButtonNotifcation/img/person.svg";
+const NavBlogRegster = () => {
+  return (
+    <Navbar expand="lg" className="rn-navbar">
+      <Navbar.Brand className="rn-logo-focal" href="#home">
+        <img src={logo} alt="Foca X" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="rn-nav">
+          <DropDown
+            name="Home"
+            option1="What is focal App"
+            option2="Who We are?"
+            option3="Lat's Talk"
+          />
+          <DropDown
+            name="Jobs"
+            option1="Manage Jobs"
+            option2="Interviews"
+            option3="Search Employees"
+          />
+          <DropDown
+            name="Freelance"
+            option1="Search Project"
+            option2="Search Services"
+            option3="Add Project"
+          />
+          <DropDown
+            name="Blog"
+            option1="Search Blog"
+            option2="Add Blog"
+            option3="Saved Blogs"
+          />
+          <DropDown name="Internship" />
+          <DropDown name="Ads" />
+          <Nav.Link href="/">Hir Us</Nav.Link>
+        </Nav>
+        <ButtonNotifcation icon={icon} />
+        <ButtonLanguge />
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBlogRegster;
